b0bcd30f84715e4ed0de3ba0e0a3e981ac6d7d67
IKSANOVA <iksanova@gmail.com>
2021-08-19 22:52:56 +0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -3,11 +3,11 @@ import time
 
 browser = Chrome()
 browser.get('https://qastand.valhalla.pw/login')
+print(f"{browser.current_url}")
 email = browser.find_element_by_name('email')
 email.send_keys('qa_test@test.ru')
 password = browser.find_element_by_name('password')
 password.send_keys('!QAZ2wsx')
 buttom = browser.find_element_by_class_name('button.is-block.is-info.is-large.is-fullwidth')
 buttom.click()
-time.sleep(20)
 browser.close()
