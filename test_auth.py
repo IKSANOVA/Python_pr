e45eab00bad3ab1da062234e8ac15a2fe1633b48
iksanova <iksanova@gmail.com>
2021-08-19 23:46:18 +0300

    test-auth

---
+++ test_auth.py
@@ -0,0 +1,14 @@
+from selenium.webdriver import Chrome
+import time
+
+def test_auth_page():
+    with Chrome() as browser:
+        browser.get('https://qastand.valhalla.pw/login')
+        print(f"{browser.current_url}")
+        email = browser.find_element_by_name('email')
+        email.send_keys('qa_test@test.ru')
+        password = browser.find_element_by_name('password')
+        password.send_keys('!QAZ2wsx')
+        buttom = browser.find_element_by_class_name('button.is-block.is-info.is-large.is-fullwidth')
+        buttom.click()
+        browser.close()
