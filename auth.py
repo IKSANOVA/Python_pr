f83e1d31e8805960d7e4c463d09fc2e898ef2e3e
IKSANOVA <iksanova@gmail.com>
2021-08-28 22:04:42 +0300

    Update auth.py
    fix

--- auth.py
+++ auth.py
@@ -1,13 +1,13 @@
 from selenium.webdriver import Chrome
-import time
 
-browser = Chrome()
-browser.get('https://qastand.valhalla.pw/login')
-print(f"{browser.current_url}")
-email = browser.find_element_by_name('email')
-email.send_keys('qa_test@test.ru')
-password = browser.find_element_by_name('password')
-password.send_keys('!QAZ2wsx')
-buttom = browser.find_element_by_class_name('button.is-block.is-info.is-large.is-fullwidth')
-buttom.click()
-browser.close()
+
+def test_auth_page():
+    with Chrome() as browser:
+        browser.get('https://qastand.valhalla.pw/login')
+        print(f"{browser.current_url}")
+        email = browser.find_element_by_name('email')
+        email.send_keys('qa_test@test.ru')
+        password = browser.find_element_by_name('password')
+        password.send_keys('!QAZ2wsx')
+        buttom = browser.find_element_by_class_name('button')
+        buttom.click()
